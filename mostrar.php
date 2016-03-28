64855af6215ee3d5fcd3747ba702f46697162416
pablo adiliberti <pablo.adiliberti@hotmail.com>
2016-03-28 19:54:00 -0300

    POST y GET

--- mostrar.php
+++ mostrar.php
@@ -3,7 +3,17 @@
 echo "Recibdo:<br>";
 
 
-var_dump($_POST);
-var_dump($_GET);
+
+/*var_dump($_POST); //trae los datos de la array encriptado , no ay limite de extencion, trea solo el name , no se puede guardar en historial 
+echo"<br>";*/
+
+/*var_dump($_GET);// trae los datos de la arrar no ecriptado , no t podes estender de 2048 caracteres , trae solo en mane, si se puede guardar en historial
+echo"<br>";*/
+
+$nombre = $_POST['usuario'];// trea el usuario del html y lo guarda en un variable php
+
+echo $nombre;
+
+
 
  ?>
